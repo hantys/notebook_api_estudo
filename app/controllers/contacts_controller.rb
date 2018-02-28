743f4bdc6c00378053c4c4cb908eef99349c5929
hantys <pedro.fausto@hotmail.com>
2018-02-28 18:04:03 -0300

    testes de status!

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,7 +5,7 @@ class ContactsController < ApplicationController
   def index
     @contacts = Contact.all
 
-    render json: @contacts
+    render json: @contacts, status: :ok
   end
 
   # GET /contacts/1
